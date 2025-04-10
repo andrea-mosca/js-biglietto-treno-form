93a54d3db36ac09309bb9be26c8218ad67cf1e20
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-10 16:51:54 +0200

    milestone 2

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const utentKm = document.getElementById("utent-km");
+const utentAge = document.getElementById("utent-age");
+const sendInfoButton = document.getElementById("send-information-button");
+
+const ticket = document.getElementById("ticket");
+const discountAmount = document.getElementById("sconto-applicato");
+const ticketCode = document.getElementById("codice-biglietto");
+const finalTicketPrice = document.getElementById("prezzo-biglietto");
+
+sendInfoButton.addEventListener("click", function(event){
+   event.preventDefault();
+   ticket.classList.remove("d-none");
+
+   const km = parseInt(utentKm.value);
+   const age = parseInt(utentAge.value);
+
+   const eurForKm = km * 0.21;
+   let ticketPrice = eurForKm;
+   const underageDiscount = ticketPrice - (ticketPrice * 0.2);
+   const elderlyDiscount = ticketPrice - (ticketPrice * 0.4);
+   let discount = "prezzo intero";
+   let randomTicketCode = Math.floor(Math.random() * 9000)+ 10000;
+   if(age < 18){
+      ticketPrice = ticketPrice - underageDiscount;
+      discount = underageDiscount;
+     } else if(age > 65){
+      ticketPrice = ticketPrice - elderlyDiscount;
+      discount = elderlyDiscount;
+     }
+   
+   finalTicketPrice.textContent = `${ticketPrice.toFixed(2)} $`;
+   ticketCode.textContent = randomTicketCode;
+   if(typeof discount === "number"){
+      discountAmount.textContent = `${discount.toFixed(2)} $`;
+   } else{
+      discountAmount.textContent = discount;
+   }
+  
+})
+
